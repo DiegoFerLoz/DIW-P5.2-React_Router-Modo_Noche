5c0ca73d16a831d676d4692570879a828e709e7d
Diego <diegofernandezl2001@gmail.com>
2025-02-24 13:43:24 +0100

    Añadido de fuente de texto

--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
   },
 ];
 
@@ -31,6 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        
       </head>
       <body>
         {children}
